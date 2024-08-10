0d8293e7090748996fd09cc268191d86e07a8881
CalElAn <calvinelikem@gmail.com>
2024-08-09 19:26:21 -0600

    Add docker deployment scripts

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,6 +4,7 @@ set -e
 git pull
 docker compose -f docker-compose.prod.yml up -d --build
 docker compose -f docker-compose.prod.yml exec python python manage.py migrate
-docker compose -f docker-compose.prod.yml exec npm  bash -c "npm install && npm run build-no-tsc"
+docker compose -f docker-compose.prod.yml exec npm  sh -c "npm install && npm run build-no-tsc"
+docker compose -f docker-compose.prod.yml exec python python manage.py collectstatic
 
 touch curriculum-mapping-tool/wsgi.py
