d0d442474e09ebea2409d456e9c90be3d95bfb47
CalElAn <calvinelikem@gmail.com>
2024-08-09 21:15:39 -0600

    update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -5,7 +5,7 @@ git pull
 docker compose -f docker-compose.prod.yml up -d --build
 docker compose -f docker-compose.prod.yml exec python python manage.py migrate
 docker compose -f docker-compose.prod.yml run --rm -it npm sh -c "npm install && npm run build"
-docker compose -f docker-compose.prod.yml exec python python manage.py collectstatic
+docker compose -f docker-compose.prod.yml exec python python manage.py collectstatic --noinput --clear
 
 # to restart the server https://stackoverflow.com/a/60833629/14324308
-kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
+docker compose -f docker-compose.prod.yml exec python kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
