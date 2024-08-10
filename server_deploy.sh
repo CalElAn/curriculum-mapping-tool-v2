7ab2fa44020423e312e194129294b05926fd5e93
CalElAn <calvinelikem@gmail.com>
2024-08-09 22:29:52 -0600

    Update server_deploy.sh to make kill command run

--- server_deploy.sh
+++ server_deploy.sh
@@ -7,5 +7,5 @@ docker compose -f docker-compose.prod.yml exec python python manage.py migrate
 docker compose -f docker-compose.prod.yml run --rm -it npm sh -c "npm install && npm run build"
 docker compose -f docker-compose.prod.yml exec python python manage.py collectstatic --noinput --clear
 
-# to restart the server https://stackoverflow.com/a/60833629/14324308
-docker compose -f docker-compose.prod.yml exec python kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
+# to restart the server https://stackoverflow.com/a/60833629/14324308 #single quotes matter here
+docker compose -f docker-compose.prod.yml exec python sh -c 'kill -HUP `ps -C gunicorn fch -o pid | head -n 1`'
